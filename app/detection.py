5b1f59c9e0c60980f8b6cee997974bf622ea0788
Ofek Nourian <ooffeekkii@gmail.com>
2025-06-17 20:45:06 +0300

    lint profed the code

--- app/detection.py
+++ app/detection.py
@@ -1,5 +1,16 @@
+"""
+This module is used as utilities functions that relate to image detection
+"""
+
+
 def detect_objects(image_path):
-    # Placeholder: return dummy result
+    """
+    Currently placeholder
+    In the future it will activate ultralytics object detection
+    and return a json of the detection
+    :param image_path:
+    :return: Json of detected objects
+    """
     return [
         {"label": "person", "confidence": 0.98, "bbox": [100, 100, 200, 200]},
         {"label": "car", "confidence": 0.89, "bbox": [300, 150, 450, 300]},
--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-from .detection import detect_objects
+"""
+This file is for serving an HTML
+and handle object detection requests for images
+"""
 import shutil
 import uuid
 import os
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from .detection import detect_objects
 
 app = FastAPI()
 
@@ -13,11 +17,20 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def root():
+    """
+    Currently a filler,
+    In the future it will provide the HTML website
+    """
     return {"message": "Object Detection API is running."}
 
 
 @app.post("/detect/")
 async def detect(file: UploadFile = File(...)):
+    """
+    function that will activate object detection on the input
+    :param file: a File
+    :return: Json of the detected objects
+    """
     file_id = str(uuid.uuid4())
     file_path = os.path.join(UPLOAD_DIR, f"{file_id}_{file.filename}")
 
